edadc3f0cc5e576353b3ddb207ad8fc7557b4fc3
Christopher Haklar <christopher.haklar.613@my.csun.edu>
2020-11-23 05:31:02 -0800

    Update app.py to test build

--- app.py
+++ app.py
@@ -2,12 +2,18 @@
 Main application file
 """
 from flask import Flask
+import logging
 app = Flask(__name__)
+
+# Initialize Logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "Breaking the unit test"
+    LOGGER.info('Received a message: %s', random_string)
+    return "".join(reversed(random_string))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
